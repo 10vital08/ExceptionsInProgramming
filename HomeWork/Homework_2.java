c20fb1b7363cf16358efbcfcfea7bf985b93af75
10vital08 <vital.lushnikov@mail.ru>
2023-04-14 21:26:49 +0300

    HM_2 + 2-3 task completed

--- HomeWork/Homework_2.java
+++ HomeWork/Homework_2.java
@@ -8,7 +8,7 @@ public class Homework_2 {
     // Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), 
     // и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения, 
     // вместо этого, необходимо повторно запросить у пользователя ввод данных.
-
+    /*
     public static float floatWrite(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите дробное число");
@@ -32,4 +32,45 @@ public class Homework_2 {
         }
         
     }
+    */
+
+
+
+    
+    // Задание 2 - поправить код
+    /*
+    try {
+        int d = 0;
+        double catchedRes1 = intArray[8] / d;
+        System.out.println("catchedRes1 = " + catchedRes1);
+     } catch (ArithmeticException e) {  // или ClassCastException, потому что double на int делим?
+        System.out.println("Catching exception: " + e);
+     }
+     */
+
+
+
+
+     // Задание 3 - поправить код
+     /* 
+     public static void main(String[] args) throws Exception {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        }
+     }
+     public static void printSum(Integer a, Integer b) throws IllegalArgumentException {
+        System.out.println(a + b);
+     }
+     */
 }
