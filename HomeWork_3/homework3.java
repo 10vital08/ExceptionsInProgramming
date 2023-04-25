ae937952a20f0a36f8a73b7ded1f2015dc4f0477
10vital08 <vital.lushnikov@mail.ru>
2023-04-25 10:50:43 +0300

    add split

--- HomeWork_3/homework3.java
+++ HomeWork_3/homework3.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class homework3 {
 
+    // опрос данных пользователя
     public static String pollingData(){
         Scanner iOScanner = new Scanner(System.in);
         String dataString = iOScanner.nextLine();
@@ -11,9 +12,20 @@ public class homework3 {
         return dataString;
     }
 
+    // разбиение данных на поля
+    public static String[] stringSplitting(String dataWords){
+        String[] words = dataWords.split(" ");
+        return words;
+    } 
+
     public static void main(String[] args) {
-        System.out.println("Введите данные о себе: ");
-        // String data = new String();
+        System.out.println("Введите данные о себе через пробел в виде 'Фамилия Имя Отчество датарождения номертелефона пол':");
         String data = pollingData();
+        String[] dataPeaples = stringSplitting(data);
+        
+        System.out.println(data);
+        // for(String word : dataPeaples){
+        //     System.out.println(word);
+        // }
     }
 }
